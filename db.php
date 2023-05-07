95b8f11b0d9694c53c112a9fbdc94372b5036636
ysh1th <98638840+ysh1th@users.noreply.github.com>
2023-05-07 10:58:08 +0400

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,20 @@
+//<?php
+
+//$dbhost = "localhost";
+//$dbusername = "root";
+//$dbpassword = "";
+//$dbname = "faculty_eval";
+//$connection = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
+//if (!$connection) {
+//    die("Connection error: " . mysqli_connect_error());
+// }
+
 <?php
 
-$dbhost = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "faculty_eval";
+$dbhost = "eu-cdbr-west-03.cleardb.net";
+$dbusername = "b970acd7548f7a";
+$dbpassword = "688cf7c2";
+$dbname = " b3897e115a7553d";
 $connection = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
 if (!$connection) {
     die("Connection error: " . mysqli_connect_error());
