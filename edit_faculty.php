a1847cc7b7e4baa07f0df183e8b8e36242e5062f
ysh1th <98638840+ysh1th@users.noreply.github.com>
2023-05-07 10:41:57 +0400

    Update edit_faculty.php

--- edit_faculty.php
+++ edit_faculty.php
@@ -136,7 +136,6 @@ if (isset($_POST['submit'])) {
         <button onclick="goBack()" class="btn btn-secondary mb-3">Go Back</button>
     </div>
 
-    <!-- Bootstrap and JavaScript -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
