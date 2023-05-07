2f95a144c8021b97c459f70dd8f6b489699ff50b
ysh1th <98638840+ysh1th@users.noreply.github.com>
2023-05-07 10:39:50 +0400

    Update student_info.php

--- student_info.php
+++ student_info.php
@@ -76,7 +76,6 @@ $students_data = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
     </div>
 
-    <!-- Bootstrap and JavaScript -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
