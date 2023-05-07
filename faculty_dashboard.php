01d3e91b4ef344aa2542612bf12b07deb5cb9f7a
ysh1th <98638840+ysh1th@users.noreply.github.com>
2023-05-07 10:42:11 +0400

    Update faculty_dashboard.php

--- faculty_dashboard.php
+++ faculty_dashboard.php
@@ -100,7 +100,8 @@ $semesters = ['sem1', 'sem2'];
     <!-- Add Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script></body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+
     <script>
         function goBack() {
             window.history.back();
