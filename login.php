b45dbce388b5aa4233518ecb13e8df653fa775c5
ysh1th <98638840+ysh1th@users.noreply.github.com>
2023-05-07 10:41:23 +0400

    Update login.php

--- login.php
+++ login.php
@@ -150,7 +150,7 @@ function test_input($data) {
         </div>
     </div>
 
-    <!-- Bootstrap and JavaScript -->
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
