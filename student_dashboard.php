31309f55fda497fba8f6f1103df7e0eed1c34687
ysh1th <98638840+ysh1th@users.noreply.github.com>
2023-05-07 10:42:20 +0400

    Update student_dashboard.php

--- student_dashboard.php
+++ student_dashboard.php
@@ -123,9 +123,9 @@ foreach ($coursesTaken as $course) {
         <button onclick="goBack()" class="btn btn-secondary mb-3">Go Back</button>
 
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
     <script>
         function goBack() {
             window.history.back();
