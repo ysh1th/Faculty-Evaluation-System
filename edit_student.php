a084fc03f4edc5537755384e7f106bed547a0a99
ysh1th <98638840+ysh1th@users.noreply.github.com>
2023-05-07 10:42:06 +0400

    Update edit_student.php

--- edit_student.php
+++ edit_student.php
@@ -105,7 +105,6 @@ if (isset($_POST['submit'])) {
         </form>
     </div>
 
-    <!-- Bootstrap and JavaScript -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
