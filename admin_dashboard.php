8ce139638aef14a7a78156b42f941b90644bfe58
ysh1th <98638840+ysh1th@users.noreply.github.com>
2023-05-07 10:42:02 +0400

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -48,7 +48,6 @@ $_SESSION['admin_name'] = $adminName;
 
     </div>
 
-    <!-- Bootstrap and JavaScript -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
