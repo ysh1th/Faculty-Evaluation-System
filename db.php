18f057880de361031fc6538abd8b19ab02874ed3
ysh1th <98638840+ysh1th@users.noreply.github.com>
2023-05-07 10:58:23 +0400

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,4 @@
-//<?php
 
-//$dbhost = "localhost";
-//$dbusername = "root";
-//$dbpassword = "";
-//$dbname = "faculty_eval";
-//$connection = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
-//if (!$connection) {
-//    die("Connection error: " . mysqli_connect_error());
-// }
 
 <?php
 
